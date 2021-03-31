ce78cca295745c4fbd351cd3516b5ec28bd45bbb
Richie <richphinsalah6@gmail.com>
2021-03-31 16:05:20 -0100

    first commit

---
+++ src/components/userform.js
@@ -0,0 +1,49 @@
+import React,{ useState } from "react";
+
+
+function Userform(props) {
+
+    const[state,userstate]=useState({
+        Username:"",
+        Email:"",
+        Country:"",
+        Password:"",
+    })
+
+    function handleonChange(event) {
+        userstate({
+            ...state,
+            [event.target.name]:event.target.value,
+        })
+    }
+
+    function handleonSubmit() {
+        props.adduser(state);
+    }
+
+    return(
+        <div >
+            <div className="row" className="form">
+                    <label className="formlabel">Username</label><span className="required">*</span><br/>
+                    <input type="text" name="Username" value={state.Username} className="inputfield" onChange={handleonChange}/>
+            </div>
+            <div className="row" className="form">
+                    <label  className="formlabel">Email</label><span className="required">*</span><br/>
+                    <input type="Email" name="Email" value={state.Email} className="inputfield" onChange={handleonChange}/>
+            </div>
+            <div className="row" className="form">
+                    <label  className="formlabel">Country</label><br/>
+                    <input type="text" name="Country" value={state.Country} className="inputfield" onChange={handleonChange}/>
+            </div>
+            <div className="row" className="form">
+                    <label  className="formlabel">Password</label><span className="required">*</span><br/>
+                    <input type="Password" name="Password" value={state.Password} className="inputfield" onChange={handleonChange}/>
+            </div><br/>
+            <div className="row" className="form">
+                <button type="Submit" className="createbtn" onClick={handleonSubmit}>Create User</button>
+                 
+            </div>
+        </div>
+    )
+}
+export default Userform;
